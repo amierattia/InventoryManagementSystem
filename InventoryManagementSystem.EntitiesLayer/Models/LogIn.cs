db9b70dbf6af9f16f4fbfb7df2ba2686be4aa690
Eng Mohammed Attia <111884096+amierattia@users.noreply.github.com>
2024-10-05 08:19:53 +0300

    Add files via upload

---
+++ InventoryManagementSystem.EntitiesLayer/Models/LogIn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.EntitiesLayer.Models
+{
+    public class LogIn
+    {
+        [Required(ErrorMessage = "البريد الإلكتروني مطلوب")]
+        [EmailAddress(ErrorMessage = "البريد الإلكتروني غير صحيح")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "كلمة المرور مطلوبة")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "تذكرني")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ InventoryManagementSystem.EntitiesLayer/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.EntitiesLayer.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription{ get; set; }
+        public decimal Price { get; set; }
+        public int stockQuantity { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+        public ICollection<Stock> Stocks { get; set; }
+    }
+}
