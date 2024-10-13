94728d81e7688bc424720b5092b51ba2757dfa74
Miro707 <148657060+Miro707@users.noreply.github.com>
2024-10-13 15:11:11 +0300

    update Authentication and Authorization

--- InventoryManagementSystem.BLL/Dto/UserRolesDto.cs
+++ InventoryManagementSystem.BLL/Dto/UserRolesDto.cs
@@ -9,13 +9,9 @@ namespace InventoryManagementSystem.BLL.Dto
 
     public class UserRolesDto
     {
-
-
         public string UserName { get; set; }
         public string UserId { get; set; }
         public List<RoleDto> Roles { get; set; }
-
-
     }
 
 }
